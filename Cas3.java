6d0d8773b8495c9017290301322b5f2e0f868671
Muslixxx <ivannvasic05@gmail.com>
2025-01-04 02:58:13 +0100

    Update Cas3.java

--- Cas3.java
+++ Cas3.java
@@ -1,28 +1,33 @@
-public class Cas3 extends Scenario1 {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cas3 extends Scenario2 {
 
     public Cas3(int nb_verificateurs) {
         super(nb_verificateurs);
 
-        this.verfificateurs = new String[]{
+        Map<String, String> result1 = melangerCritèresEtReponses(
                 "• Critère A : Le lieu est La Poudrière ou Axone.\n" +
                         "• Critère B : Le lieu est Foyer Montbéliard ou MDE Sevenans.\n" +
                         "• Critère C : Le lieu est Foyer Belfort uniquement.\n",
+                "La Poudrière ou Axone"
+        );
 
+        Map<String, String> result2 = melangerCritèresEtReponses(
                 "• Critère A : L’organisateur est CrunchTime ou Gala.\n" +
                         "• Critère B : L’organisateur est AE ou SkiUT.\n" +
                         "• Critère C : L’organisateur est BDS uniquement.\n",
+                "CrunchTime ou Gala"
+        );
 
-                "• Critère A : Le nombre d’invités est ≥ 100.\n" +
-                        "• Critère B : Le nombre d’invités est ≤ 150.\n" +
-                        "• Critère C : Le nombre d’invités est < 50.\n",
+        this.verfificateurs = new String[]{
+                result1.get("verificateur"),
+                result2.get("verificateur")
+        };
 
-                "• Critère A : Si le lieu est La Poudrière, les invités doivent être ≥ 150.\n" +
-                        "• Critère B : Si le lieu est Axone, les invités doivent être < 100.\n" +
-                        "• Critère C : Si le lieu est MDE Sevenans, les invités doivent être ≥ 300.\n",
-
-                "• Critère A : Si l’organisateur est CrunchTime, le lieu est La Poudrière ou Axone.\n" +
-                        "• Critère B : Si l’organisateur est AE, le lieu est un Foyer.\n" +
-                        "• Critère C : Si l’organisateur est Gala, le lieu est Axone uniquement.\n"
+        this.correctChoices = new String[]{
+                result1.get("correctChoice"),
+                result2.get("correctChoice")
         };
 
         this.Final_answer = new String[]{"La Poudrière", "CrunchTime", "200"};
