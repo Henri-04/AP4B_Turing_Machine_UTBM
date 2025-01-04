c2311f053c865f5319554e014b0bcd63adc87477
Muslixxx <ivannvasic05@gmail.com>
2025-01-04 02:57:49 +0100

    Update Cas1.java

--- Cas1.java
+++ Cas1.java
@@ -1,32 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
 
-public class Cas1 extends Scenario1{
+public class Cas1 extends Scenario1 {
 
-    public Cas1(int nb_verificateurs){
-
-
+    public Cas1(int nb_verificateurs) {
         super(nb_verificateurs);
 
-
-        this.verfificateurs = new String[] {
+        Map<String, String> result1 = melangerCritèresEtReponses(
                 "• Critère A : Le lieu est un Foyer.\n" +
                         "• Critère B : Le lieu est Axone ou La Poudrière.\n" +
                         "• Critère C : Le lieu est MDE Sevenans uniquement.\n",
+                "Axone ou La Poudrière"
+        );
 
+        Map<String, String> result2 = melangerCritèresEtReponses(
                 "• Critère A : L’organisateur est BDS ou CrunchTime.\n" +
                         "• Critère B : L’organisateur est AE uniquement.\n" +
                         "• Critère C : L’organisateur est Gala ou SkiUT.\n",
+                "BDS ou CrunchTime"
+        );
 
-                "• Critère A : Le nombre d’invités est ≤ 100.\n" +
-                        "• Critère B : Le nombre d’invités est > 200.\n" +
-                        "• Critère C : Le nombre d’invités est < 50.\n"
+        this.verfificateurs = new String[]{
+                result1.get("verificateur"),
+                result2.get("verificateur")
         };
 
+        this.correctChoices = new String[]{
+                result1.get("correctChoice"),
+                result2.get("correctChoice")
+        };
 
-
-        this.Final_answer = new String[]{"Foyer Belfort","BDS","75"};
-
+        this.Final_answer = new String[]{"Foyer Belfort", "BDS", "75"};
     }
-
-
-
 }
