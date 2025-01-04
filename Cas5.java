35353de1351de7a45621bb6ed74f7067b05bd2b9
Muslixxx <ivannvasic05@gmail.com>
2025-01-04 02:58:39 +0100

    Update Cas5.java

--- Cas5.java
+++ Cas5.java
@@ -1,32 +1,33 @@
-public class Cas5 extends Scenario1 {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cas5 extends Scenario3 {
 
     public Cas5(int nb_verificateurs) {
         super(nb_verificateurs);
 
-        this.verfificateurs = new String[]{
+        Map<String, String> result1 = melangerCritèresEtReponses(
                 "• Critère A : Le lieu est MDE Sevenans ou La Poudrière.\n" +
                         "• Critère B : Le lieu est Foyer Montbéliard ou Axone.\n" +
                         "• Critère C : Le lieu est Foyer Belfort uniquement.\n",
+                "MDE Sevenans ou La Poudrière"
+        );
 
+        Map<String, String> result2 = melangerCritèresEtReponses(
                 "• Critère A : L’organisateur est SkiUT ou AE.\n" +
                         "• Critère B : L’organisateur est CrunchTime uniquement.\n" +
                         "• Critère C : L’organisateur est Gala ou BDS.\n",
+                "SkiUT ou AE"
+        );
 
-                "• Critère A : Le nombre d’invités est ≥ 100 et ≤ 150.\n" +
-                        "• Critère B : Le nombre d’invités est ≥ 200.\n" +
-                        "• Critère C : Le nombre d’invités est < 50.\n",
+        this.verfificateurs = new String[]{
+                result1.get("verificateur"),
+                result2.get("verificateur")
+        };
 
-                "• Critère A : Si le lieu est MDE Sevenans, l’organisateur est SkiUT ou CrunchTime.\n" +
-                        "• Critère B : Si le lieu est La Poudrière, l’organisateur est AE uniquement.\n" +
-                        "• Critère C : Si le lieu est Axone, l’organisateur est BDS.\n",
-
-                "• Critère A : Si les invités sont 120, le lieu est MDE Sevenans ou Axone.\n" +
-                        "• Critère B : Si les invités sont 200, le lieu est La Poudrière.\n" +
-                        "• Critère C : Si les invités sont 350, le lieu est un Foyer.\n",
-
-                "• Critère A : Si l’organisateur est SkiUT, le nombre d’invités doit être ≥ 100 et ≤ 150.\n" +
-                        "• Critère B : Si l’organisateur est CrunchTime, le nombre d’invités doit être < 100.\n" +
-                        "• Critère C : Si l’organisateur est AE, le nombre d’invités doit être > 300.\n",
+        this.correctChoices = new String[]{
+                result1.get("correctChoice"),
+                result2.get("correctChoice")
         };
 
         this.Final_answer = new String[]{"MDE Sevenans", "SkiUT", "120"};
