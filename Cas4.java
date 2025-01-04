140408173a1e0a68750a05b5b7dc70de0bffd580
Muslixxx <ivannvasic05@gmail.com>
2025-01-04 12:13:52 +0100

    Update Cas4.java

--- Cas4.java
+++ Cas4.java
@@ -1,44 +1,44 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Cas3 extends Scenario2 {
+public class Cas4 extends Scenario2 {
 
-    public Cas3(int nb_verificateurs) {
+    public Cas4(int nb_verificateurs) {
         super(nb_verificateurs);
 
         Map<String, String> result1 = melangerCritèresEtReponses(
-                "• Critère A (vrai) : Le lieu est La Poudrière ou Axone.\n" +
-                        "• Critère B : Le lieu est Foyer Montbéliard ou MDE Sevenans.\n" +
-                        "• Critère C : Le lieu est Foyer Belfort uniquement.\n",
-                "La Poudrière ou Axone"
+                "• Critère A (vrai) : Le lieu est La Poudrière ou Foyer Montbéliard.\n" +
+                        "• Critère B : Le lieu est Foyer Belfort ou MDE Sevenans.\n" +
+                        "• Critère C : Le lieu est Axone uniquement.\n",
+                "La Poudrière ou Foyer Montbéliard"
         );
 
         Map<String, String> result2 = melangerCritèresEtReponses(
-                "• Critère A (vrai) : L’organisateur est CrunchTime ou Gala.\n" +
-                        "• Critère B : L’organisateur est AE ou SkiUT.\n" +
-                        "• Critère C : L’organisateur est BDS uniquement.\n",
-                "CrunchTime ou Gala"
+                "• Critère A (vrai) : L’organisateur est BDS ou Gala.\n" +
+                        "• Critère B : L’organisateur est AE ou CrunchTime.\n" +
+                        "• Critère C : L’organisateur est SkiUT uniquement.\n",
+                "BDS ou Gala"
         );
 
         Map<String, String> result3 = melangerCritèresEtReponses(
-                "• Critère A (vrai) : Le nombre d’invités est ≥ 100.\n" +
-                        "• Critère B : Le nombre d’invités est ≤ 150.\n" +
-                        "• Critère C : Le nombre d’invités est < 50.\n",
-                "≥ 100"
+                "• Critère A (vrai) : Le nombre d’invités est < 150.\n" +
+                        "• Critère B : Le nombre d’invités est > 250.\n" +
+                        "• Critère C : Le nombre d’invités est ≥ 150.\n",
+                "< 150"
         );
 
         Map<String, String> result4 = melangerCritèresEtReponses(
-                "• Critère A (vrai) : Si le lieu est La Poudrière, les invités doivent être ≥ 150.\n" +
-                        "• Critère B : Si le lieu est Axone, les invités doivent être < 100.\n" +
-                        "• Critère C : Si le lieu est MDE Sevenans, les invités doivent être ≥ 300.\n",
-                "≥ 150"
+                "• Critère A (vrai) : Si le lieu est La Poudrière, l’organisateur est BDS ou Gala.\n" +
+                        "• Critère B : Si le lieu est Foyer Montbéliard, l’organisateur est AE.\n" +
+                        "• Critère C : Si le lieu est Axone, l’organisateur est SkiUT.\n",
+                "BDS ou Gala"
         );
 
         Map<String, String> result5 = melangerCritèresEtReponses(
-                "• Critère A (vrai) : Si l’organisateur est CrunchTime, le lieu est La Poudrière ou Axone.\n" +
-                        "• Critère B : Si l’organisateur est AE, le lieu est un Foyer.\n" +
-                        "• Critère C : Si l’organisateur est Gala, le lieu est Axone uniquement.\n",
-                "La Poudrière ou Axone"
+                "• Critère A (vrai) : Si le nombre d’invités est 100, le lieu est La Poudrière.\n" +
+                        "• Critère B : Si le nombre d’invités est 200, le lieu est MDE Sevenans.\n" +
+                        "• Critère C : Si le nombre d’invités est < 100, le lieu est Axone.\n",
+                "La Poudrière"
         );
 
         this.verfificateurs = new String[]{
@@ -57,6 +57,6 @@ public class Cas3 extends Scenario2 {
                 result5.get("correctChoice")
         };
 
-        this.Final_answer = new String[]{"La Poudrière", "CrunchTime", "200"};
+        this.Final_answer = new String[]{"La Poudrière", "BDS", "100"};
     }
 }
