9be94a23a4ad652219df93b664fb5d7910363f56
Muslixxx <ivannvasic05@gmail.com>
2025-01-04 02:58:25 +0100

    Update Cas4.java

--- Cas4.java
+++ Cas4.java
@@ -1,28 +1,33 @@
-public class Cas4 extends Scenario1 {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cas4 extends Scenario2 {
 
     public Cas4(int nb_verificateurs) {
         super(nb_verificateurs);
 
-        this.verfificateurs = new String[]{
+        Map<String, String> result1 = melangerCritèresEtReponses(
                 "• Critère A : Le lieu est La Poudrière ou Foyer Montbéliard.\n" +
                         "• Critère B : Le lieu est Foyer Belfort ou MDE Sevenans.\n" +
                         "• Critère C : Le lieu est Axone uniquement.\n",
+                "La Poudrière ou Foyer Montbéliard"
+        );
 
+        Map<String, String> result2 = melangerCritèresEtReponses(
                 "• Critère A : L’organisateur est BDS ou Gala.\n" +
                         "• Critère B : L’organisateur est AE ou CrunchTime.\n" +
                         "• Critère C : L’organisateur est SkiUT uniquement.\n",
+                "BDS ou Gala"
+        );
 
-                "• Critère A : Le nombre d’invités est < 150.\n" +
-                        "• Critère B : Le nombre d’invités est > 250.\n" +
-                        "• Critère C : Le nombre d’invités est ≥ 150.\n" ,
+        this.verfificateurs = new String[]{
+                result1.get("verificateur"),
+                result2.get("verificateur")
+        };
 
-                "• Critère A : Si le lieu est La Poudrière, l’organisateur est BDS ou Gala.\n" +
-                        "• Critère B : Si le lieu est Foyer Montbéliard, l’organisateur est AE.\n" +
-                        "• Critère C : Si le lieu est Axone, l’organisateur est SkiUT.\n",
-
-                "• Critère A : Si le nombre d’invités est 100, le lieu est La Poudrière.\n" +
-                        "• Critère B : Si le nombre d’invités est 200, le lieu est MDE Sevenans.\n" +
-                        "• Critère C : Si le nombre d’invités est < 100, le lieu est Axone.\n"
+        this.correctChoices = new String[]{
+                result1.get("correctChoice"),
+                result2.get("correctChoice")
         };
 
         this.Final_answer = new String[]{"La Poudrière", "BDS", "100"};
